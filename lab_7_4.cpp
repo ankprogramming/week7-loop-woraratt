58fe49d5f6a809c8f58f50838ea298899f223996
Unknown <mymintzata@hotmail.co.th>
2018-01-22 17:50:55 +0700

    ข้อที่ 7.4 เสร็จ

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -2,45 +2,58 @@
 
 using namespace std;
 
-int main () {
+int main ()
+{
 
-  // Without loop
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
+    // Without loop
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    cout << "****" << endl;
+    cout << "*****" << endl;
+    cout << "******" << endl;
+    cout << "*****" << endl;
+    cout << "****" << endl;
+    cout << "***" << endl;
+    cout << "**" << endl;
+    cout << "*" << endl;
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    cout << "****" << endl;
+    cout << "*****" << endl;
+    cout << "******" << endl;
+    cout << "*****" << endl;
+    cout << "****" << endl;
+    cout << "***" << endl;
+    cout << "**" << endl;
+    cout << "*" << endl;
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    cout << "****" << endl;
+    cout << "*****" << endl;
+    cout << "******" << endl;
+    cout << "*****" << endl;
+    cout << "****" << endl;
+    cout << "***" << endl;
+    cout << "**" << endl;
+    cout << "*" << endl;
 
-  // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
+    // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "**" << endl;
+        cout << "***" << endl;
+        cout << "****" << endl;
+        cout << "*****" << endl;
+        cout << "******" << endl;
+        cout << "*****" << endl;
+        cout << "****" << endl;
+        cout << "***" << endl;
+        cout << "**" << endl;
+        cout << "*" << endl;
+    }
 
-
-  return 0;
+    return 0;
 }
