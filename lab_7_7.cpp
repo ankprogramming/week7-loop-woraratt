5d19a217828047d573685927bd16ec59cadc88e2
Unknown <mymintzata@hotmail.co.th>
2018-01-22 18:23:01 +0700

    ข้อที่ 7.6 เสร็จ

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -5,6 +5,18 @@ using namespace std;
 int main () {
 
   // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
+  int n = 0;
+  int sum = 0;
+  float avg = 0 ;
+  for (int i=0; i < 5; i++)
+  {
+        cin >> n;
+        sum += n;
+  }
+
+   //cout << sum;
+   avg = (float) sum / 5;
+   cout << avg << endl;
 
   return 0;
 }
